0f75df19aafb13e40a504c1f1677e79c57f60c59
WoW Amaze So Amazing <kostadin.belchev@abv.bg>
2020-12-10 16:02:43 +0200

    Cleaned out the unnecessary structure. Changed - NextBestMove to do MinMax

--- src/MKAgent/KalahPlayer.java
+++ src/MKAgent/KalahPlayer.java
@@ -12,7 +12,8 @@ public class KalahPlayer {
     private int holes;
     private int maxDepth = 7;
 
-    public KalahPlayer(int holes, int seeds) {
+    public KalahPlayer(int holes, int seeds)
+    {
         this.ourSide = Side.SOUTH;
         this.holes = holes;
         this.kalah = new Kalah(new Board(holes, seeds));
@@ -32,21 +33,48 @@ public class KalahPlayer {
         return ourSeeds - oppSeeds;
     }
 
-    protected int bestNextMove()
+    public int bestNextMove(Side givenSide, int maxDepth, int currentDepth, int holes, Kalah kalah, Board board)
     {
+        currentDepth ++;
+        // If we have reached the max given depth we evaluate the current board and start going back
+        if (currentDepth == maxDepth || Kalah.gameOver(board))
+            return BoardEvaluator.evaluateBoard(board, this.ourSide);
 
-        // Set the root node
-        Node rootNode = new Node(this.kalah.getBoard());
-        rootNode.setDepth(0);
-        rootNode.setSide(ourSide);
+        int currentBestMove = 0;
+        int ourValue = 0;
+        // For every possible move at current possition
+        for (int i = 1; i <= holes; i++)
+        {
+            Move move = new Move(givenSide, i);
+            Board boardNew = new Board(board);
 
-        //generate the tree
-        GameTree.generateTree(rootNode, this.ourSide, maxDepth, this.holes, this.kalah, this.ourSide);
+            // Check if the move is legal
+            if (kalah.isLegalMove(boardNew, move))
+            {
+                // If it is, make it, create a new which has the state
+                //of the board and do it again
 
-        //GameTree.writeBoardsToAFileBFS(rootNode);
+                int branchValue = bestNextMove(Kalah.makeMove(boardNew, move), maxDepth, currentDepth, holes, kalah,
+                       boardNew);
+                if (currentBestMove == 0)
+                {
+                    currentBestMove = i;
+                    ourValue = branchValue;
+                }//if
+                else if((this.ourSide == givenSide && ourValue < branchValue) ||
+                                                                (this.ourSide != givenSide && ourValue > branchValue))
+                {
+                    currentBestMove = i;
+                    ourValue = branchValue;
+                }//if
+            }//if
+        }//for
 
-        return rootNode.getBestMove();
-    }
+        if(currentDepth == 0)
+            return currentBestMove;
+        else
+            return ourValue;
+    }//bestNextMove
 
     protected void swap() {
         this.ourSide = this.ourSide.opposite();
@@ -63,7 +91,8 @@ public class KalahPlayer {
                 if (Protocol.interpretStartMsg(msg)) {
                     this.ourSide = Side.SOUTH;
                     Main.sendMsg(Protocol.createMoveMsg(1));
-                } else {
+                } else
+                    {
                     this.ourSide = Side.NORTH;
                     maySwap = true;
                 }
@@ -86,7 +115,8 @@ public class KalahPlayer {
 
                     if (moveTurn.again && !moveTurn.end) {
                         msg = null;
-                        int nextMove = this.bestNextMove();
+                        int nextMove = this.bestNextMove(this.ourSide, maxDepth, -1,this.holes, this.kalah,
+                                this.kalah.getBoard());
                         if (maySwap) {
                             Board moveBoard = new Board(this.kalah.getBoard());
                             Kalah.makeMove(moveBoard, new Move(this.ourSide, nextMove));
