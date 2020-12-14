c69d5ce14673b8a84aaa6e0383e72a1c9c3496eb
WoW Amaze So Amazing <kostadin.belchev@abv.bg>
2020-12-14 12:44:14 +0200

    Changed structure of board Evaluator

--- src/MKAgent/BoardEvaluator.java
+++ src/MKAgent/BoardEvaluator.java
@@ -4,52 +4,67 @@ package MKAgent;
 public class BoardEvaluator
 {
 
-     static int relationWeight = 1;
-     static int percentageWeight = 0;
+    public static int ourMancalaWeight = 5;
+    public static int enemyMancalaWeight = -1;
+    public static int ourHolesWeight = 0;
+    public static int enemyHolesWeight = 0;
+    public static int ourPercentageWeight = 0;
+    public static int enemyPercentageWeight = 0;
 
 
     // Returns an int which is or heuristic
     public static int evaluateBoard(Board board, Side ourSide)
     {
-        return   (evaluateBoardRelation(board,ourSide) * relationWeight)
-               + (evaluateMancalaPercentage(board, ourSide) * percentageWeight);
+        return    (ourMancalaEvaluate(board,ourSide)           * ourMancalaWeight)
+                + (enemyMancalaEvaluate(board, ourSide)        * enemyMancalaWeight)
+                + (ourHolesEvaluate(board, ourSide)            * ourHolesWeight)
+                + (enemyHolesEvaluate(board, ourSide)          * enemyHolesWeight)
+                + (ourMancalaPercentage(board, ourSide)        * ourPercentageWeight)
+                + (enemyMancalaPercentage(board, ourSide)      * enemyPercentageWeight);
+
 
     }//evaluateBoard
 
-
-    public static int evaluateBoardRelation(Board board, Side ourSide)
+    public static int ourMancalaEvaluate(Board board, Side ourSide)
     {
-        int mankalahModifier = 2;
-        int seedsModifier = 1;
+        return board.getSeedsInStore(ourSide);
+    }//myMancalaEvaluate
 
+    public static int enemyMancalaEvaluate(Board board, Side ourSide)
+    {
+        return -board.getSeedsInStore(ourSide.opposite());
+    }
+
+    public static int ourHolesEvaluate(Board board, Side ourSide)
+    {
         int finalScoreOur = 0;
-        int finalScoreEnemy = 0;
         for(int i = 1; i <= board.getNoOfHoles(); i++)
         {
             finalScoreOur += board.getSeeds(ourSide, i);
+        }//for
+
+        return finalScoreOur;
+    }
+
+    public static int enemyHolesEvaluate(Board board, Side ourSide)
+    {
+        int finalScoreEnemy = 0;
+        for(int i = 1; i <= board.getNoOfHoles(); i++)
+        {
             finalScoreEnemy += board.getSeeds(ourSide.opposite(), i);
         }//for
 
-        finalScoreOur *= seedsModifier ;
-        finalScoreEnemy *= seedsModifier ;
-
-        finalScoreOur += board.getSeedsInStore(ourSide)*mankalahModifier;
-        finalScoreEnemy += board.getSeedsInStore(ourSide.opposite())*mankalahModifier;
-
-
-        return finalScoreOur - finalScoreEnemy;
+        return -finalScoreEnemy;
     }
 
-    public static int evaluateMancalaPercentage(Board board, Side ourSide)
+    public static int ourMancalaPercentage(Board board, Side ourSide)
     {
-        int ourPercentage = board.getSeedsInStore(ourSide) * 98 / 100;
-        int enemyPercentage = board.getSeedsInStore(ourSide.opposite()) * 98 / 100;
-
-        if(ourPercentage > enemyPercentage)
-            return ourPercentage;
-        else
-            return 0 - enemyPercentage;
+        return board.getSeedsInStore(ourSide) * 98 / 100;
     }//evaluateBoardPercentage
 
+    public static int enemyMancalaPercentage(Board board, Side ourSide)
+    {
+        return -board.getSeedsInStore(ourSide.opposite()) * 98 / 100;
+    }
 
 }
