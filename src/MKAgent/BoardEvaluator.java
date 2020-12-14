f871c1d80eef0c00c77c2cd9a7a114322f15e3b9
michaelkrasa <michael.krasa@gmail.com>
2020-12-14 23:52:57 +0100

    Simplified some heuristics, more ideas ready for implementation

--- src/MKAgent/BoardEvaluator.java
+++ src/MKAgent/BoardEvaluator.java
@@ -3,47 +3,33 @@ package MKAgent;
 // Class that is concerned with Evaluating the board.
 public class BoardEvaluator {
 
-    public static int ourMancalaWeight = 5;
-    public static int enemyMancalaWeight = -1;
-    public static int ourHolesWeight = 0;
-    public static int enemyHolesWeight = 0;
-    public static int ourPercentageWeight = 0;
-    public static int enemyPercentageWeight = 0;
+    public static int mancalaWeight = 2;
+    public static int holesWeight = 1;
+    public static int ourPercentageWeight = 1;
+    public static int enemyPercentageWeight = 1;
 
     // Returns an int which is or heuristic
     public static int evaluateBoard(Board board, Side ourSide) {
-        return    (ourMancalaEvaluate(board,ourSide)           * ourMancalaWeight)
-                + (enemyMancalaEvaluate(board, ourSide)        * enemyMancalaWeight)
-                + (ourHolesEvaluate(board, ourSide)            * ourHolesWeight)
-                + (enemyHolesEvaluate(board, ourSide)          * enemyHolesWeight)
+        return    (mancalaEvaluate(board,ourSide)              * mancalaWeight)
+                + (holesEvaluate(board, ourSide)               * holesWeight)
+                + repeatMovePossible(board, ourSide)
                 + (ourMancalaPercentage(board, ourSide)        * ourPercentageWeight)
                 + (enemyMancalaPercentage(board, ourSide)      * enemyPercentageWeight);
     } // evaluateBoard
 
-    public static int ourMancalaEvaluate(Board board, Side ourSide) {
-        return board.getSeedsInStore(ourSide);
+    public static int mancalaEvaluate(Board board, Side ourSide) {
+        return board.getSeedsInStore(ourSide) - board.getSeedsInStore(ourSide.opposite());
     }
 
-    public static int enemyMancalaEvaluate(Board board, Side ourSide) {
-        return -board.getSeedsInStore(ourSide.opposite());
-    }
-
-    public static int ourHolesEvaluate(Board board, Side ourSide) {
+    public static int holesEvaluate(Board board, Side ourSide) {
         int finalScoreOur = 0;
+        int finalScoreEnemy = 0;
+
         for(int i = 1; i <= board.getNoOfHoles(); i++) {
             finalScoreOur += board.getSeeds(ourSide, i);
-        }
-
-        return finalScoreOur;
-    }
-
-    public static int enemyHolesEvaluate(Board board, Side ourSide) {
-        int finalScoreEnemy = 0;
-        for(int i = 1; i <= board.getNoOfHoles(); i++) {
             finalScoreEnemy += board.getSeeds(ourSide.opposite(), i);
         }
-
-        return -finalScoreEnemy;
+        return finalScoreOur - finalScoreEnemy;
     }
 
     public static int ourMancalaPercentage(Board board, Side ourSide) {
@@ -53,4 +39,23 @@ public class BoardEvaluator {
     public static int enemyMancalaPercentage(Board board, Side ourSide) {
         return -board.getSeedsInStore(ourSide.opposite()) * 98 / 100;
     }
+
+
+    // Check for Kalah-move - read in Kalah.java
+    public static int repeatMovePossible(Board board, Side side) {
+        int seedsInPot;
+        int moves = 0;
+        Board copy = new Board(board);
+
+        for (int i = 1; i <= board.getNoOfHoles(); i++) {
+            seedsInPot = board.getSeeds(side, i);
+            if (Kalah.makeMove(copy, new Move(side, i)) == side) moves++;
+            copy = new Board(board);
+        }
+        return moves;
+    }
+
+
+    // TODO which stones can be captured
+    //public static int capturedEnemyStones(Board board, Side side);
 }
